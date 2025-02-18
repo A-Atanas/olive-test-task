2311066c7864a02123ceffad13f0cafccfb84eae
aatanasov-aurorasolar <aatanasov@aurorasolar.com>
2025-02-18 20:49:12 +0200

    WIP: added walking button, but still need to figure out camera rotation

--- src/app/action-menu.tsx
+++ src/app/action-menu.tsx
@@ -3,15 +3,43 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import styles from "./styles/action-menu.module.css";
 import { Actions, ActionsProps, Items } from "./types";
-import { SWEEP_IN_OFFICE_ID } from "./constants";
+import { STARTING_SWEEP, SWEEP_IN_OFFICE_ID } from "./constants";
+import { Vector2 } from "three";
 
 const ActionMenu = ({sdk}: ActionsProps) => {
     const [open, setOpen] = useState(false);
     const [items, setItems] = useState<Items>();
 
     const actions: Actions = useMemo(() => ({
-        teleport: (sdk) => {sdk.Sweep.moveTo(SWEEP_IN_OFFICE_ID, {})},
-        walk: (sdk) => {}
+        teleport: (sdk) => {
+            sdk.Sweep.moveTo(SWEEP_IN_OFFICE_ID, {transition: sdk.Sweep.Transition.INSTANT})
+        },
+        walk: async (sdk) => {
+            const sweepGraph = await sdk.Sweep.createGraph();
+            const startSweep = sweepGraph.vertex(STARTING_SWEEP)!;
+            const endSweep = sweepGraph.vertex(SWEEP_IN_OFFICE_ID)!;
+            for (const { src, dst, weight } of sweepGraph.edges) {
+                sweepGraph.setEdge({ src, dst, weight: weight ** 2 });
+            }
+
+            const path = sdk.Graph.createAStarRunner(sweepGraph, startSweep, endSweep).exec().path;
+            sdk.Camera.pose.subscribe(function (pose) {
+                // Changes to the Camera pose have occurred.
+                console.log('Current position is ', pose.position);
+                console.log('Rotation angle is ', pose.rotation);
+                console.log('Sweep UUID is ', pose.sweep);
+                console.log('View mode is ', pose.mode);
+              });
+              
+            for (const vertex of path) {
+                await sdk.Camera.setRotation({
+                    x: vertex.data.position.x,
+                    y: vertex.data.position.y
+                });
+                await sdk.Sweep.moveTo(vertex.id, {transition: sdk.Sweep.Transition.FLY});
+            }
+            sweepGraph.dispose();
+        }
     }), [])
 
     const toggleMenu = useCallback(() => {
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,3 @@
 export const SWEEP_WITH_BANANA_ID = "665pcnn0eaxz2x80xb473gg3b";
 export const SWEEP_IN_OFFICE_ID = "xcqt6xq986fwi7gxnendg42ca";
+export const STARTING_SWEEP = "2sdesz1hz3s2rqi982bw4xuxd";
