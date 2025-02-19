f0bc3b3b2b88b243fc4fef84bd3e782167acbec4
Антон <anton@MacBook-Pro-Anton.local>
2025-02-19 14:48:39 +0200

    refactoring: hide menu button if not in inside mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from "./styles/page.module.css";
 import { ShowcaseBundleWindow, MpSdk } from "../../public/sdk";
 import ActionMenu from "./action-menu";
 import { setupTagAndModel } from "./setup";
+import { Mode } from "@matterport/sdk";
 
 export default function Home() {
   const [sdk, setSdk] = useState<MpSdk>();
@@ -25,8 +26,11 @@ export default function Home() {
       }
 
       await setupTagAndModel(mpSdk);
+
       await mpSdk.App.state.waitUntil(state => state.phase === mpSdk.App.Phase.PLAYING);
-      setShowMenu(true);
+      mpSdk.Mode.current.subscribe((mode) => {
+        setShowMenu(mode === "mode.inside");
+      });
     });
   }, []);
 
